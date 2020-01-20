966bfb6992fe46b9f3ed9e4d3077c68bc5a134c5
Rick Glascock <rixong@gmail.com>
2020-01-20 04:17:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i=0
+  while i < source_array.length
+    new.push(yield(source_array[i]))
+    i+=1
+  end
+  return new
+end
+
+def reduce(source_array, sv=nil)
+ if sv
+  cur_total = sv
+  i = 0
+ else
+  cur_total = source_array[0]
+  i = 1
+ end
+  while i < source_array.length
+    cur_total = yield(cur_total, source_array[i] )
+    i += 1
+  end
+  cur_total
+end
