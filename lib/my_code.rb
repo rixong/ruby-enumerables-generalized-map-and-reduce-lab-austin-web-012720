7c125df2a5fb93be070f6a5a253d98a61d5aed5d
Rick Glascock <rixong@gmail.com>
2020-01-20 03:34:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i=0
+  while i < source_array.length
+    new.push(yield(source_array[i]))
+    i+=1
+  end
+  return new
+end
+
+def reduce(source_array, total = 0)
+  i=0
+  while i < source_array.length
+    total = yield(source_array[i], total)
+    i += 1
+  end
+  return total
+end
